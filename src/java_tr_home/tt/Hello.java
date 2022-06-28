9b2eb364f87454cde090f74d5188872467164948
pinetreelch <chanho.lee0215@gmail.com>
2022-06-28 13:40:18 +0900

    c

---
+++ src/java_tr_home/tt/Hello.java
@@ -0,0 +1,11 @@
+package java_tr_home.tt;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!");
+		
+	}
+
+}
